a9a076f4d943e54a3bf8319eb200503a7e6f4c14
Andrey Kuznetsov <8150309@gmail.com>
2025-02-10 19:11:19 +0300

    Initial Commit

---
+++ BombGame/BombGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombGameApp.swift
+//  BombGame
+//
+//  Created by Андрей Кузнецов on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BombGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
