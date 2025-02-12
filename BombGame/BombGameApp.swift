9a9f76d13d4fe5c2aa7d9bba7139274a1b96ddbb
NickKhachtryan <nickoarm@mail.ru>
2025-02-12 12:10:41 +0300

    conflict resolved

--- BombGame/BombGameApp.swift
+++ BombGame/BombGameApp.swift
@@ -10,11 +10,10 @@ import SwiftUI
 @main
 struct BombGameApp: App {
     var body: some Scene {
-//        var viewModel = CategoryViewModel()
+        var viewModel = CategoryViewModel()
         WindowGroup {
-//            CategoryView(viewModel: viewModel)
-//                .environmentObject(viewModel)
-            MainView()
+            CategoryView(viewModel: viewModel)
+                .environmentObject(viewModel)
         }
     }
 }
