b835d6c5abd15c77159f1251197e0a3979f1a144
NickKhachtryan <nickoarm@mail.ru>
2025-02-14 00:30:12 +0300

    singleton pattern for punishments is done

--- BombGame/Punishments/PunishmentsViewModel.swift
+++ BombGame/Punishments/PunishmentsViewModel.swift
@@ -10,8 +10,16 @@ import Foundation
 class PunishmentsViewModel: ObservableObject {
 
     
+    //MARK: - PROPERTIES
+    static let shared = PunishmentsViewModel()
+    
+    
+    //MARK: - INITIALIZATORS
+    private init() {}
+
+    
     //MARK: - METHODS
-    func getRandomPunishment() -> String{
+    func getRandomPunishment() -> String {
         let allPunishments = PunishmentsModel.allCases
         let randomIndex = Int.random(in: 0..<allPunishments.count)
         return allPunishments[randomIndex].rawValue
